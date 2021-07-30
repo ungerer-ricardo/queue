c09b2d98de219b695617242011f97f2f0a6275b3
Ricardo Ungerer <ungerer.ricardo@gmail.com>
2021-07-30 12:56:30 +0100

    Initial Commit

---
+++ queue/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  queue
+//
+//  Created by Ricardo Ungerer on 30/07/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
